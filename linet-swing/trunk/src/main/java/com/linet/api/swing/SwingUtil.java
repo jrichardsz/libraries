52f17097c64ad977eedc121f9d0430ccdfb09093
jrichardsz <import.linet@gmail.com>
2013-05-08 10:29:57 -0500

    Jtree changed util

--- linet-swing/trunk/src/main/java/com/linet/api/swing/SwingUtil.java
+++ linet-swing/trunk/src/main/java/com/linet/api/swing/SwingUtil.java
@@ -7,16 +7,12 @@ package com.linet.api.swing;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.net.URL;
-import java.util.Enumeration;
+
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTree;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeNode;
-import javax.swing.tree.TreePath;
 
 /**
  *
@@ -24,7 +20,7 @@ import javax.swing.tree.TreePath;
  */
 public class SwingUtil {
 
-    public static void centrarJDialog(JDialog jDialog) {
+    public static void centerJDialog(JDialog jDialog) {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenSize = toolkit.getScreenSize();
         int x = (screenSize.width - jDialog.getWidth()) / 2;
@@ -32,7 +28,7 @@ public class SwingUtil {
         jDialog.setLocation(x, y);
     }
     
-    public static void centrarJFrame(JFrame  jFrame) {
+    public static void centerJFrame(JFrame  jFrame) {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenSize = toolkit.getScreenSize();
         int x = (screenSize.width - jFrame.getWidth()) / 2;
@@ -40,37 +36,6 @@ public class SwingUtil {
         jFrame.setLocation(x, y);
     }    
 
-    public static String getNameNodoAtIndex(int ind, JTree jtree) {
-
-        TreePath parentPath = jtree.getSelectionPath();
-        //System.out.println("parentPath:" + parentPath);
-        int nivel = 0;
-        if (parentPath != null) {
-
-            nivel = parentPath.getPathCount();
-        }
-
-        return "" + parentPath.getPathComponent(ind);
-    }
-
-    public static String getNameNodoPadreAnterior(JTree tree) {
-        TreePath parentPath = tree.getSelectionPath();
-        return "" + parentPath.getParentPath().getLastPathComponent();
-    }
-
-    public static DefaultMutableTreeNode buscarNodeByNombre(JTree jtree,String nodeStr) {
-        DefaultMutableTreeNode node = null;
-        TreeNode root = (TreeNode)jtree.getModel().getRoot();
-        Enumeration e = root.children();
-        while (e.hasMoreElements()) {
-            node = (DefaultMutableTreeNode) e.nextElement();
-            if ((node.getUserObject().toString()).contains(nodeStr)) {
-                return node;
-            }
-        }
-        return null;
-    }
-    
     public static void showDialogWhitImage(URL pathuRL){
         
         JPanel panel = new JPanel();
