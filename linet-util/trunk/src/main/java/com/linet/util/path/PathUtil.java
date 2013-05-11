44d35fd1d816561891d235a43ad3dab72d23aecc
Richard <import.liner@gmail.com>
2013-05-11 00:38:09 -0500

    get web inf path!!

---
+++ linet-util/trunk/src/main/java/com/linet/util/path/PathUtil.java
@@ -0,0 +1,46 @@
+package com.linet.util.path;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.linet.util.file.FileUtil;
+import com.linet.util.so.OSValidator;
+
+public class PathUtil {
+
+	
+	public static String getPathFromWhereApplicationIsRunning() {
+        String path = null;
+        try {
+            path = new File(".").getCanonicalPath();
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return path;
+    }
+	
+    public static String getWebInfPath() {
+
+        String WEBINF = "WEB-INF";
+        String filePath = "";
+        URL url = PathUtil.class.getResource("PathUtil.class");
+        String className = url.getFile();
+        filePath = className.substring(0, className.indexOf(WEBINF) + WEBINF.length());
+
+//        if (url.getProtocol().equals("file")) {
+//            filePath = filePath.replaceAll("file:", "");
+//        } else if (url.getProtocol().equals("jar")) {
+//        }
+
+        filePath = filePath.replaceAll("file:", "");
+        if (OSValidator.isWindows()) {
+            filePath = filePath.substring(1);
+        }
+        
+        return filePath;
+
+    }
+}
