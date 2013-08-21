dab3ffdb7a1e493e297d238191b49b2bb766cbe0
Richard <import.liner@gmail.com>
2013-08-21 01:05:33 -0500

    remove older method in FileUtil!

--- linet-util/src/main/java/com/linet/util/file/FileUtil.java
+++ linet-util/src/main/java/com/linet/util/file/FileUtil.java
@@ -13,7 +13,6 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -22,33 +21,6 @@ import java.util.Date;
 import javax.activation.MimetypesFileTypeMap;
 
 public class FileUtil {
-
-	
-    public static File getFileDirectoryEjecucion() {
-        File WORKING_DIRECTORY = null;
-        
-        if (WORKING_DIRECTORY == null) {
-            try {
-                URL url = FileUtil.class.getResource("/root.txt");
-                if (url.getProtocol().equals("file")) {
-                    File f = new File(url.toURI());
-                    f = f.getParentFile().getParentFile().getParentFile();
-                    WORKING_DIRECTORY = f;
-                } else if (url.getProtocol().equals("jar")) {
-                    String expected = "!/root.txt";
-                    String s = url.toString();
-                    s = s.substring(4);
-                    s = s.substring(0, s.length() - expected.length());
-                    File f = new File(new URL(s).toURI());
-                    f = f.getParentFile();
-                    WORKING_DIRECTORY = f;
-                }
-            } catch (Exception e) {
-                WORKING_DIRECTORY = new File(".");
-            }
-        }
-        return WORKING_DIRECTORY;
-    }
 
     public static String getPathFromWhereApplicationIsRunning() throws Exception {
         String path = null;
